516f79d6c2094c2fd1cbd611d9b7000f77bf6f37
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-07-31 17:07:06 -0700

    Used ListView instead of RecyclerView + indeterminate progress bar

--- app/src/main/java/com/example/android/latestintechnews/MainActivity.java
+++ app/src/main/java/com/example/android/latestintechnews/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.android.latestintechnews;
 
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Intent;
 import android.content.Loader;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,23 +22,44 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     private static final int ARTICLE_LOADER_ID = 1;
     private static final String GUARDIAN_URL_REQUEST =
             "https://content.guardianapis.com/search?q=technology&order-by=newest&show-tags=contributor&api-key=0bd554a4-e9c9-4ec8-89e3-7b34fb23d284";
+    private TextView mEmptyStateTextView;
+    private ListView articleListView;
+    private ProgressBar mProgressSpinner;
 
     /**
-     * Create the MainActivity.
+     * Load the MainActivity, set adapter and onItemClickListener for adapter
+     * items, start loader for network request.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is to set up the RecyclerView.
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        RecyclerView articleListView = findViewById(R.id.list);
-        articleListView.setLayoutManager(linearLayoutManager);
+        // Initialize UI variables.
+        articleListView = findViewById(R.id.list);
+        mEmptyStateTextView = findViewById(R.id.empty_state);
+        mProgressSpinner = findViewById(R.id.loading_spinner);
 
-        //Initialize and set adapter.
+        // Set empty view.
+        articleListView.setEmptyView(mEmptyStateTextView);
+
+        // Initialize and set adapter.
         mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
         articleListView.setAdapter(mAdapter);
+
+        // When article is clicked, open a new intent to view the article in a web browser.
+        articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Article currentArticle = mAdapter.getItem(position);
+                Uri articleUri = Uri.parse(currentArticle.getWebUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, articleUri);
+
+                if (websiteIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(websiteIntent);
+                }
+            }
+        });
 
         //Start the loader.
         LoaderManager loaderManager = getLoaderManager();
@@ -53,8 +79,9 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
      */
     @Override
     public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
+        mProgressSpinner.setVisibility(View.GONE);
         mAdapter.clear();
-
+        mEmptyStateTextView.setText(R.string.no_results);
         if (articles != null && !articles.isEmpty()) {
             mAdapter.addAll(articles);
         }
